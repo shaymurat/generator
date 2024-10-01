4cb7c7f06dc56137a1ab2d8a7b65eb72e02679a5
Ildar Shaymurat <shaymuratoff@ya.ru>
2024-10-02 00:02:11 +0500

    first commit

---
+++ module_9_6.py
@@ -0,0 +1,15 @@
+def all_variants(text):
+    '''
+    генераторная функция
+    выдаёт подстроки всё возрастающей длины
+    '''
+    for i in range(len(text)):
+        for j in range(len(text) - i):
+            yield text[j:j+i+1]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+print(tuple(all_variants('абвгд')))
